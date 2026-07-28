fb76fc1aab656bce56a92a5d8931509ddfd95950
EHoon <inlays_sun_0y@icloud.com>
2026-07-28 15:03:26 +0900

    chore: 앱 표시 이름 콘솔과 일치화 '나의 여름방학 일기' -> '나의 여름방학일기'

--- src/constants/brand.ts
+++ src/constants/brand.ts
@@ -1,4 +1,4 @@
-export const BRAND_DISPLAY_NAME = "나의 여름방학 일기";
+export const BRAND_DISPLAY_NAME = "나의 여름방학일기";
 export const BRAND_PRIMARY_COLOR = "#dccb8a";
 
 // `brand.icon` in granite.config.ts takes an image URL, never a repo path: the
